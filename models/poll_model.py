32b47d2a8d7ec7b15f9fc50461a65a591936795a
Sanjay Mathew <sanjay.mathewofficial2020@gmail.com>
2025-10-26 08:41:19 +0530

    feat: updated poll model to auto version increment when poll options are added to poll

--- models/poll_model.py
+++ models/poll_model.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 
-from sqlalchemy import Column, String, Integer, DateTime, func
+from sqlalchemy import Column, String, Integer, DateTime, func, event
 from sqlalchemy.orm import Mapped, relationship
 from sqlalchemy.testing.schema import mapped_column
 
-from core.base import VersionedMixin, Base
+from core.base import VersionedMixin, Base, update_version_id_on_change
 from models.poll_options_model import PollOptions
 
 
@@ -16,4 +16,5 @@ class Poll(VersionedMixin, Base):
     likes: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now() ,nullable=False)
 
-
+event.listens_for(Poll.poll_options, "append")(update_version_id_on_change)
+event.listens_for(Poll.poll_options, "remove")(update_version_id_on_change)
